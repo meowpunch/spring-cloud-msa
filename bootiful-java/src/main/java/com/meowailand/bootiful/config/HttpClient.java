0feae326467308c0c96fb5ca84a71b3acd28b640
meowpunch <jinhoonbae@gmail.com>
2021-04-23 16:30:20 +0900

    JB: test

--- bootiful-java/src/main/java/com/meowailand/bootiful/config/HttpClient.java
+++ bootiful-java/src/main/java/com/meowailand/bootiful/config/HttpClient.java
@@ -1,9 +1,14 @@
 package com.meowailand.bootiful.config;
 
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 /**
  * 
@@ -16,6 +21,16 @@ public class HttpClient {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpRequestFactory.setConnectTimeout(1000);
+		httpRequestFactory.setReadTimeout(1000);
+		org.apache.http.client.HttpClient httpClient = HttpClientBuilder.create()
+				.setMaxConnTotal(200)
+				.setMaxConnPerRoute(20)
+				.build();
+		httpRequestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+		restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+		return restTemplate;
 	}
 }
